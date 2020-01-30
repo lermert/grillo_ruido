412b1c80f4d13961f56fa52fb39beb46f68a5dd2
lermert <laura.ermert@posteo.net>
2020-01-30 17:37:55 -0500

    second commit

---
+++ grillo_spectrograms.py
@@ -0,0 +1,62 @@
+from obspy import read, UTCDateTime
+import os
+from matplotlib.pyplot import cm
+
+# some input
+files = ['mx.004.MSEED',
+         'mx.001.MSEED']
+freq_min = 0.05  # 0.1
+freq_max = 1.0  # 10.0
+corners = 10
+zerophase = False
+window_length_for_spectrogram = 1200  # seconds
+window_length_for_fft = 30  # seconds
+minimum_length = 0  # seconds
+mult = 2  # padding for spectrogram
+olap = 0.75
+# clips of plot colorscale
+lower_clip = 0  # 0.25
+upper_clip = 1  # 0.75
+log_freq = True
+t0 = UTCDateTime("2020-01-28T19:10:00")
+t1 = UTCDateTime("2020-01-28T20:00:00")
+
+
+def apply_filter(trace):
+    for windowed_tr in trace.slide(window_length=3600.0, step=1800.0):
+        windowed_tr.detrend('demean')
+        windowed_tr.detrend('linear')
+    trace.taper(0.02)
+    trace.filter('bandpass', freqmin=freq_min, freqmax=freq_max,
+                 corners=corners, zerophase=zerophase)
+
+
+for file in files:
+    tr = read(file)
+    tr = tr.trim(starttime=t0, endtime=t1)
+    apply_filter(tr)
+    print(tr)
+    for component in ['ENX', 'ENY', 'ENZ']:
+        tr_p = tr.select(channel=component)
+
+        for tr_pp in tr_p.slide(window_length=window_length_for_spectrogram,
+                                step=window_length_for_spectrogram,
+                                include_partial_windows=False):
+            if len(tr_pp) == 1:
+                tr_pp = tr_pp[0]
+            else:
+                tr_pp = tr_pp.merge(fill_value=0)[0]
+            if tr_pp.stats.delta * tr_pp.stats.npts < minimum_length:
+                continue
+            outfile = os.path.splitext(file)[0]
+            of = outfile + '.' +\
+                 component + '.' +\
+                 tr_pp.stats.starttime.strftime("%Y.%m.%dT%H:%M") + '.spec.png'
+            tr_pp.spectrogram(outfile=of,
+                              cmap=cm.viridis,
+                              wlen=window_length_for_fft, mult=mult,
+                              dbscale=False,
+                              clip=[lower_clip, upper_clip],
+                              per_lap=olap,
+                              log=log_freq,
+                              freq_min=freq_min, freq_max=freq_max)
---
+++ grillo_to_mseed_2.py
@@ -0,0 +1,73 @@
+from obspy import Stream, Trace, UTCDateTime
+import json
+from glob import glob
+import os
+import numpy as np
+
+# input:
+indir = 'example_data_quake3/'
+# handling overlaps:
+interpolation_samples = 0
+# 0: Do not interpolate, n: interpolate n samples
+# -1: interpolate all overlapping samples
+misalignment_threshold = 0.5
+# 0: do no align samples, 0.5: align all samples with sub-sample shift
+
+
+def clean_up(stream_in):
+    # align subsample shifts
+    stream_in.merge(method=-1,
+                    misalignment_threshold=misalignment_threshold)
+    # close overlaps
+    # (either discarding or interpolating the overlapping samples)
+    stream_in.merge(method=1, fill_value=None,
+                    interpolation_samples=interpolation_samples)
+    s = stream_in.split()  # do not return a masked array
+    return(s)
+
+
+traces = glob(os.path.join(indir, '*jsonl'))
+traces.sort()
+station_previous = ''
+for t in traces:
+    print(t)
+    network, station = os.path.basename(t).split('.')[0: 2]
+
+    if station != station_previous:
+        if station_previous != '':
+            s_out = Stream()
+            for stream in [s_x, s_y, s_z]:
+                s_out += clean_up(stream)
+            s_out.write('{}.{}.MSEED'.format(network, station_previous),
+                        format='MSEED')
+        s_x = Stream()
+        s_y = Stream()
+        s_z = Stream()
+        station_previous = station
+    f = open(t, 'r')
+    f = f.readlines()
+
+    for rec in f:
+        d = json.loads(rec)
+        tr = Trace()
+        tr.stats.sampling_rate = d['sr']
+        tr.stats.starttime = UTCDateTime(d['device_t']) -\
+            (len(d['x']) - 1) / d['sr']
+        tr.stats.station = station
+        tr.stats.network = network
+
+        tr.data = np.array(d['x'])
+        tr.stats.channel = 'ENX'
+        s_x += tr.copy()
+        tr.data = np.array(d['y'])
+        tr.stats.channel = 'ENY'
+        s_y += tr.copy()
+        tr.data = np.array(d['z'])
+        tr.stats.channel = 'ENZ'
+        s_z += tr.copy()
+
+s_out = Stream()
+for stream in [s_x, s_y, s_z]:
+    s_out += clean_up(stream)
+
+s_out.write('{}.{}.MSEED'.format(network, station), format='MSEED')
